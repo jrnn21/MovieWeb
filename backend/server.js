9b660917815e8a0565bf11f3cf341ecac83b8315
jrnn21 <chamroeuncl22@gmail.com>
2021-06-24 16:57:39 +0700

    new

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3200;
 
 app.listen(
  PORT,
